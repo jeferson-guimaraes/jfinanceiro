c96f0c7c817cc642f0a82ce4b4e71d2a5e128ddf
Jeferson de Oliveira Guimarães <50177095+jeferson-guimaraes@users.noreply.github.com>
2025-10-30 11:26:59 -0300

    Insere traduções de mensagens do Laravel para português do Brasil

---
+++ lang/pt_BR/passwords.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Linhas de Idioma de Redefinição de Senha
+    |--------------------------------------------------------------------------
+    |
+    | As seguintes linhas de idioma são as linhas padrão que correspondem aos
+    | motivos dados pelo corretor de senha para uma tentativa de atualização
+    | de senha, como uma tentativa de token inválido ou nova senha inválida.
+    |
+    */
+
+    'reset' => 'Sua senha foi redefinida!',
+    'sent' => 'Enviamos um link de redefinição de senha para o seu e-mail!',
+    'throttled' => 'Por favor, aguarde antes de tentar novamente.',
+    'token' => 'Este token de redefinição de senha é inválido.',
+    'user' => 'Não conseguimos encontrar um usuário com esse endereço de e-mail.',
+];
