ff9671f181a592f7ff649fd576caef97d7895cce
Jeferson de Oliveira Guimarães <50177095+jeferson-guimaraes@users.noreply.github.com>
2026-05-30 11:01:01 -0300

    Refatora abordagem do componente Icon

--- resources/js/components/Icon.vue
+++ resources/js/components/Icon.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 import { cn } from '@/lib/utils';
-import * as icons from 'lucide-vue-next';
-import { computed } from 'vue';
+import { type Component, computed } from 'vue';
 
 interface Props {
-    name: string;
+    icon: Component;
     class?: string;
     size?: number | string;
     color?: string;
@@ -18,11 +17,6 @@ const props = withDefaults(defineProps<Props>(), {
 });
 
 const className = computed(() => cn('h-4 w-4', props.class));
-
-const icon = computed(() => {
-    const iconName = props.name.charAt(0).toUpperCase() + props.name.slice(1);
-    return (icons as Record<string, any>)[iconName];
-});
 </script>
 
 <template>
